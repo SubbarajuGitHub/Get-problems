91e54169074e763b463b855aaf766bd3e92416db
ABHISHANMG <123348070+ABHISHANMG@users.noreply.github.com>
2023-01-23 13:24:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-const path = require("path");
-app.use(express.json());
+const app = express();
 
 const dbPath = path.join(__dirname, "cricketTeam.db");
 
 let db = null;
 
-const initializationDatabase = async () => {
+const initializeDBAndServer = async () => {
   try {
-    const db = await open({
+    db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3000);
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`);
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
     process.exit(1);
   }
 };
-initializationDatabase();
 
-const project = () => {
-  app.get(`/players/`, async (request, response) => {
-    const playerTeam = `SELECT * FROM cricket_team;`;
-    const booksArray = await db.all(playerTeam);
-    Response.send(booksArray);
-  });
+initializeDBAndServer();
+//get players
+
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
 };
-module.exports = project;
+
+app.get("/players/", async (request, response) => {
+  const playerTeam = `SELECT * FROM cricket_team;`;
+  const playersArray = await db.all(playerTeam);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
+
+//post
+
+app.post("/players/", async (request, response) => {
+  const playerDetails = request.body;
+  const { player_id, player_name, jersey_number, role } = playerDetails;
+
+  const addPlayer = `INSERT INTO cricket_team (player_id,player_name,jersey_number,role)
+    VALUES ('${player_id}','${player_name}','${jersey_number}','${role}'
+        );`;
+
+  const dbResponse = await db.run(addPlayer);
+  const playerId = dbResponse.lastID;
+  response.send({ player_id: player_id });
+});
+
+module.exports = app;
